4768e1ae606aa937bd6d7ec2977561deeb79876b
afei <afei@55yali.com>
2014-05-21 11:11:31 +0800

    修改个人资料编辑页面样式

--- app/controllers/group.js
+++ app/controllers/group.js
@@ -810,7 +810,7 @@ exports.updateFormation = function(req, res){
       var _formation = [];
       var _tempFormation = req.body.formation;
       for (var member in _tempFormation){
-        _formation.push({'uid':member_length,
+        _formation.push({'uid':member,
                           'x':_tempFormation[member].x,
                           'y':_tempFormation[member].y
 
