ae8a58b9abf466dd359f45b3995b2676e62f584f
afei <afei@55yali.com>
2014-05-22 15:24:34 +0800

    增加邮件模板

--- app/services/mail.js
+++ app/services/mail.js
@@ -2,8 +2,10 @@
 
 var mailer = require('nodemailer'),
     encrypt = require('../middlewares/encrypt'),
-    config = require('../config/config');
-
+    config = require('../config/config'),
+    rootConfig = require('../../config/config'),
+    jade = require('jade'),
+    fs = require('fs');
 var transport = mailer.createTransport('SMTP', config.MAIL_OPTION);
 
 var SITE_ROOT_URL = 'http://127.0.0.1:3000';
@@ -30,63 +32,91 @@ exports.sendCompanyActiveMail = function (who, name, id, host) {
   var from = '动梨无限<nicoJiang@55yali.com>';
   var to = who;
   var subject = name + ' 动梨社区公司账号激活';
-  var html = '<p>您好：<p/>' +
-    '<p>我们收到您在动梨的申请信息，请点击下面的链接来激活帐户：</p>' +
-    '<a href="http://' + host + '/company/validate?key=' + encrypt.encrypt(id,config.SECRET) + '&id=' + id + '">激活链接</a>';
+  var content = '<p>我们收到您在动梨的申请信息，请点击下面的链接来激活帐户：</p>' +
+    '<a href="http://' + host + '/company/validate?key=' + encrypt.encrypt(id,config.SECRET) + '&id=' + id + '">http://' + host + '/company/validate?key=' + encrypt.encrypt(id,config.SECRET) + '&id=' + id + '</a>';
 
-  sendMail({
-    from: from,
-    to: to,
-    subject: subject,
-    html: html
-  });
+    fs.readFile(rootConfig.root+'/app/views/partials/mailTemplate.jade', 'utf8', function (err, data) {
+        if (err) throw err;
+        console.log(data);
+        var fn = jade.compile(data);
+        var html = fn({'title':'注册激活','who':who,'content':content});
+        console.log(html);
+        sendMail({
+          from: from,
+          to: to,
+          subject: subject,
+          html: html
+        });
+    });
 };
 
 exports.sendStaffActiveMail = function(who, uid, cid, host) {
   var from = '动梨无限<nicoJiang@55yali.com>';
   var to = who;
   var subject = '动梨社区员工账号激活';
-  var html = '<p>您好：<p/>' +
-    '<p>我们收到您在动梨的申请信息，请点击下面的链接来激活帐户：</p>' +
+  var content = '<p>我们收到您在动梨的申请信息，请点击下面的链接来激活帐户：</p>' +
     '<a href="http://' + host + '/users/setProfile?key=' + encrypt.encrypt(uid, config.SECRET) +
-    '&uid=' + uid + '&cid=' + cid + '">激活链接</a>';
+    '&uid=' + uid + '&cid=' + cid + '">http://' + host + '/users/setProfile?key=' + encrypt.encrypt(uid, config.SECRET) +
+    '&uid=' + uid + '&cid=' + cid + '</a>';
 
-  sendMail({
-    from: from,
-    to: to,
-    subject: subject,
-    html: html
-  });
+    fs.readFile(rootConfig.root+'/app/views/partials/mailTemplate.jade', 'utf8', function (err, data) {
+        if (err) throw err;
+        console.log(data);
+        var fn = jade.compile(data);
+        var html = fn({'title':'注册激活','who':who,'content':content});
+        console.log(html);
+        sendMail({
+          from: from,
+          to: to,
+          subject: subject,
+          html: html
+        });
+    });
 };
 exports.sendStaffResetPwdMail = function(who, uid, host) {
   var from = '动梨无限<nicoJiang@55yali.com>';
   var to = who;
   var subject = '动梨社区员工密码重置';
-  var html = '<p>您好：<p/>' +
-    '<p>我们收到您在动梨的密码重置申请信息，请点击下面的链接来重置密码（30分钟内有效）：</p>' +
+  var content = '<p>我们收到您在动梨的密码重置申请信息，请点击下面的链接来重置密码（30分钟内有效）：</p>' +
     '<a href="http://' + host + '/users/resetPwd?key=' + encrypt.encrypt(uid, config.SECRET) +
-    '&uid=' + uid +'&time='+encrypt.encrypt(new Date().toString(), config.SECRET)+'">密码重置</a>';
+    '&uid=' + uid +'&time='+encrypt.encrypt(new Date().toString(), config.SECRET)+
+    '">http://' + host + '/users/resetPwd?key=' + encrypt.encrypt(uid, config.SECRET) +
+    '&uid=' + uid +'&time='+encrypt.encrypt(new Date().toString(), config.SECRET)+'</a>';
 
-  sendMail({
-    from: from,
-    to: to,
-    subject: subject,
-    html: html
-  });
+    fs.readFile(rootConfig.root+'/app/views/partials/mailTemplate.jade', 'utf8', function (err, data) {
+        if (err) throw err;
+        console.log(data);
+        var fn = jade.compile(data);
+        var html = fn({'title':'重置密码','who':who,'content':content});
+        console.log(html);
+        sendMail({
+          from: from,
+          to: to,
+          subject: subject,
+          html: html
+        });
+    });
 };
 exports.sendCompanyResetPwdMail = function(who, uid, host) {
   var from = '动梨无限<nicoJiang@55yali.com>';
   var to = who;
   var subject = '动梨社区公司密码重置';
-  var html = '<p>您好：<p/>' +
-    '<p>我们收到您在动梨的密码重置申请信息，请点击下面的链接来重置密码（30分钟内有效）：</p>' +
+  var content = '<p>我们收到您在动梨的密码重置申请信息，请点击下面的链接来重置密码（30分钟内有效）：</p>' +
     '<a href="http://' + host + '/company/resetPwd?key=' + encrypt.encrypt(uid, config.SECRET) +
-    '&uid=' + uid +'&time='+encrypt.encrypt(new Date().toString(), config.SECRET)+'">密码重置</a>';
-
-  sendMail({
-    from: from,
-    to: to,
-    subject: subject,
-    html: html
-  });
+    '&uid=' + uid +'&time='+encrypt.encrypt(new Date().toString(), config.SECRET)+
+    '">http://' + host + '/company/resetPwd?key=' + encrypt.encrypt(uid, config.SECRET) +
+    '&uid=' + uid +'&time='+encrypt.encrypt(new Date().toString(), config.SECRET)+'</a>';
+    fs.readFile(rootConfig.root+'/app/views/partials/mailTemplate.jade', 'utf8', function (err, data) {
+        if (err) throw err;
+        console.log(data);
+        var fn = jade.compile(data);
+        var html = fn({'title':'重置密码','who':who,'content':content});
+        console.log(html);
+        sendMail({
+          from: from,
+          to: to,
+          subject: subject,
+          html: html
+        });
+    });
 };
