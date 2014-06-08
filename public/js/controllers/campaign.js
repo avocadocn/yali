7bb509ad57e726c98289384f81a94b13783c35f0
Yali-07 <maggie03230@gmail.com>
2014-06-08 19:56:36 +0800

    修改全局样式 变为固定宽度

--- public/js/controllers/campaign.js
+++ public/js/controllers/campaign.js
@@ -1,10 +1,11 @@
 'use strict';
 
-var campaignApp = angular.module('campaignApp', []);
+var campaignApp = angular.module('campaign', []);
 
 campaignApp.controller('campaignController', ['$scope', '$http', function($scope, $http) {
     $scope.joinCampaign = function (cid) {
-        return 0;
+        console.log("!!!!!!");
+        alert("!!!");
     };
     $scope.quitCampaign = function (cid) {
         return 0;
