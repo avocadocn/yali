aca69cd0e083a1f58548d038ab0a56f20efffd89
cahavar <cahavar@55yali.com>
2014-06-11 15:31:06 +0800

    添加页脚相关页面

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -5,9 +5,24 @@ exports.render = function(req, res) {
       user: req.user ? JSON.stringify(req.user) : 'null'
   });
 };
-// exports.test = function(req, res) {
-//   var host = req.headers.host;
-//   var who = 'afeiszh@gmail.com';
-//   var content = '<p>我们收到您在动梨的申请信息，请点击下面的链接来激活帐户：</p><a href="'+host+'"></a>';
-//   res.render('partials/mailTemplate', {'title':'注册激活','host':"http://"+host,'who':who,'content':content});
-// };
+
+
+exports.about = function(req, res) {
+  res.render('about');
+};
+
+exports.law = function(req, res) {
+  res.render('law');
+};
+
+exports.privacy = function(req, res) {
+  res.render('privacy');
+};
+
+exports.question = function(req, res) {
+  res.render('question');
+};
+
+exports.contact = function(req, res) {
+  res.render('contact');
+};
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,9 +1,13 @@
 'use strict';
 
 module.exports = function(app) {
-    // Home route
     var index = require('../controllers/index');
     app.get('/', index.render);
-    //app.get('/test',index.test);
+
+    app.get('/about', index.about);
+    app.get('/law', index.law);
+    app.get('/privacy', index.privacy);
+    app.get('/question', index.question);
+    app.get('/contact', index.contact);
 
 };
