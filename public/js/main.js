8c20e9d806212c5666cf6843663013438e643e37
afei <afei@55yali.com>
2014-06-12 17:37:01 +0800

    修改活动列表时间显示

--- public/js/main.js
+++ public/js/main.js
@@ -383,3 +383,47 @@ app.run(['$translate','$rootScope', function ($translate,$rootScope) {
       $rootScope.initAlertCss();
     }
 }]);
+app.filter('dateView', function() {
+return function(input) {
+  var today = new Date();
+  var date = new Date(input);
+  if(date.getFullYear()===today.getFullYear()&&date.getMonth()===today.getMonth()){
+    if(date.getDate()===today.getDate())
+      return '今天';
+    else if(date.getDate()===today.getDate()+1)
+      return '明天';
+  }
+    return input;
+}
+});
+app.filter('week', function() {
+return function(input) {
+// input will be ginger in the usage below
+switch(new Date(input).getDay()){
+  case 0:
+  input = '周日';
+  break;
+  case 1:
+  input = '周一';
+  break;
+  case 2:
+  input = '周二';
+  break;
+  case 3:
+  input = '周三';
+  break;
+  case 4:
+  input = '周四';
+  break;
+  case 5:
+  input = '周五';
+  break;
+  case 6:
+  input = '周六';
+  break;
+  default:
+  input = '';
+}
+return input;
+}
+});
