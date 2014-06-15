06e5d79ee8427c5ecfedbe50b2941b8304a88de9
afei <afei@55yali.com>
2014-06-15 13:18:54 +0800

    修改小组logo显示

--- app/controllers/company.js
+++ app/controllers/company.js
@@ -296,7 +296,7 @@ exports.groupSelect = function(req, res) {
                 companyGroup.group_type = selected_groups[i].group_type;
                 companyGroup.entity_type = selected_groups[i].entity_type;
                 companyGroup.name = tname;
-                companyGroup.logo = '/img/icons/group/' + companyGroup.entity_type +'_on.png';
+                companyGroup.logo = '/img/icons/group/' + companyGroup.entity_type.toLowerCase() +'_on.png';
 
                 companyGroup.save(function (err){
                     if (err) {
