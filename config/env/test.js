c3a8614d11d3a109095551549eab32d4cbd910d4
cahavar <cahavar@55yali.com>
2014-06-24 14:33:17 +0800

    修改数据库配置

--- config/env/test.js
+++ config/env/test.js
@@ -1,7 +1,7 @@
 'use strict';
 
 module.exports = {
-    db: 'mongodb://localhost/mean-test',
+    db: 'mongodb://127.0.0.1/donler-beta',
     port: 3001,
     app: {
         name: 'Donler 企业员工活动互联网工具'
