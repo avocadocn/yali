a5972b3d67d26ed1aaf6634db3ee43c3ef27e532
cahavar <cahavar@55yali.com>
2014-06-26 16:19:00 +0800

    修改日历视图样式

--- public/js/controllers/tabviewUser.js
+++ public/js/controllers/tabviewUser.js
@@ -315,13 +315,13 @@ tabViewUser.controller('ScheduleListController', ['$scope', '$http', '$rootScope
 
   var calendar = $('#calendar').calendar(options);
 
-  $('#calendar_operator [data-calendar-nav]').each(function() {
+  $('#calendar_nav [data-calendar-nav]').each(function() {
     var $this = $(this);
     $this.click(function() {
       calendar.navigate($this.data('calendar-nav'));
     });
   });
-  $('#calendar_operator [data-calendar-view]').each(function() {
+  $('#calendar_view [data-calendar-view]').each(function() {
     var $this = $(this);
     $this.click(function() {
       calendar.view($this.data('calendar-view'));
