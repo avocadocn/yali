b614790aee7b5b8680d9be02927ce86fa852a2be
Yali-07 <maggie03230@gmail.com>
2014-08-01 17:55:24 +0800

    timeline照片传出数改为4，公司小队页样式修改

--- app/controllers/company.js
+++ app/controllers/company.js
@@ -986,7 +986,7 @@ exports.timeLine = function(req, res){
           start_time: campaign.start_time,
           provoke:campaign.camp.length>0,
           year: getYear(campaign),
-          photo_list: photo_album_controller.photoThumbnailList(campaign.photo_album, 10)
+          photo_list: photo_album_controller.photoThumbnailList(campaign.photo_album, 4)
         }
         // todo new time style
         // console.log(campaign);
