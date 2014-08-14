a4b9d5f03242895c9c7d0519b607b86560ca8279
cahavar <cahavar@55yali.com>
2014-08-14 16:15:38 +0800

    解决app滚动问题

--- public/mobile/www/js/controllers.js
+++ public/mobile/www/js/controllers.js
@@ -613,13 +613,12 @@ angular.module('starter.controllers', [])
     scope.initUpload();
   };
 })
-.directive('noScroll', function($document) {
 
+.directive('noScroll', function() {
   return {
     restrict: 'A',
-    link: function($scope, $element, $attr) {
-
-      $document.on('touchmove', function(e) {
+    link: function(scope, element, attr) {
+      element.on('touchmove', function(e) {
         e.preventDefault();
       });
     }
