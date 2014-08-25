cfa9fd6e3c6bf4c6cd88e664654ba5d5a0965d18
cahavar <cahavar@55yali.com>
2014-08-25 19:12:28 +0800

    添加新的图片服务

--- app/controllers/image.js
+++ app/controllers/image.js
@@ -11,7 +11,7 @@ var gm = require('gm');
 var config = require('../../config/config');
 
 
-exports.resize = function(req, res) {
+exports.resizeWithCrop = function(req, res) {
   var uri_dir = req.params[0];
   var target_width = Number(req.params[2]);
   var target_height = Number(req.params[3]);
@@ -73,3 +73,70 @@ exports.resize = function(req, res) {
 
 
 };
+
+exports.resizeWithoutCrop = function(req, res) {
+  var uri_dir = req.params[0];
+  var target_width = Number(req.params[2]);
+  var target_height = Number(req.params[3]);
+  var stretch = req.params[4];
+
+  if (isNaN(target_width) || isNaN(target_height)) {
+    return res.send(400);
+  }
+  if (target_width < 0 || target_height < 0) {
+    return res.send(400);
+  }
+
+  var file_path = path.join(config.root, 'public', uri_dir);
+
+  if (!fs.existsSync(file_path)) {
+    return res.send(404);
+  }
+
+  var sendImg = function(err, stdout, stderr) {
+    if (err) {
+      console.log(err);
+      res.send(500);
+    }
+    else {
+      stdout.pipe(res);
+    }
+  };
+
+  if (stretch) {
+    gm(file_path)
+    .resize(target_width, target_height)
+    .stream(sendImg);
+  } else {
+    gm(file_path)
+    .size(function(err, value) {
+      var ori_width = value.width;
+      var ori_height = value.height;
+
+      // tw/th - ow/oh => (tw*oh-th*ow)/(th*oh), 和0比较可忽略分母
+      var compare_result = target_width * ori_height - target_height * ori_width;
+
+      if (compare_result < 0) {
+
+        var resize_width = target_width;
+
+        gm(file_path)
+        .resize(resize_width)
+        .stream(sendImg);
+
+      } else {
+
+        var resize_height = target_height;
+
+        gm(file_path)
+        .resize(null, resize_height)
+        .stream(sendImg);
+
+      }
+
+    });
+  }
+
+
+};
+
