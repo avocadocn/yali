f107ccda9e56a2075fb431d20474590714b6eb20
cahavar <cahavar@55yali.com>
2014-09-12 17:40:00 +0800

    发邮件时，如果用户在webpower数据库中不存在，则创建

--- app/services/webpower.js
+++ app/services/webpower.js
@@ -6,7 +6,7 @@ var encrypt = require('../middlewares/encrypt');
 
 var website_config = require('../config/config');
 
-var config = {
+var global_config = {
   wsdl: 'http://donler.dmdelivery.com/x/soap-v3/wsdl.php',
   login: {
     username: 'cahavar',
@@ -20,15 +20,26 @@ var config = {
   }
 };
 
-
-
-exports.sendStaffResetPwdMail = function (email, uid, host, callback) {
+/**
+ * 发送邮件
+ * @param  {Object} config webpower的配置
+ * config: {
+ *   login: {
+ *     username: String,
+ *     password: String
+ *   },
+ *   campaignID: Int,
+ *   mailingID: Int
+ * }
+ * @param  {String} email  目标邮箱
+ * @param  {Array} fields 添加的用户数据, [{name:'...', value:'...'}, {...}, ...]
+ * @param  {Function} callback callback(err)
+ */
+var sendMail = function (config, email, fields, callback) {
 
   var end_callback = callback;
 
-  var reset_link = 'http://' + host + '/users/resetPwd?key=' + encrypt.encrypt(uid, website_config.SECRET) + '&uid=' + uid + '&time=' + encrypt.encrypt(new Date().toString(), website_config.SECRET);
-
-  soap.createClient(config.wsdl, function (err, client) {
+  soap.createClient(global_config.wsdl, function (err, client) {
 
     if (err) { return console.log(err); }
 
@@ -56,35 +67,52 @@ exports.sendStaffResetPwdMail = function (email, uid, host, callback) {
         });
       },
       function (recipient, callback) {
-        // TO DO: 修改用户的重置密码链接(还没处理没有找到该用户的情况)
-        client.editRecipient({
-          login: config.login,
-          campaignID: config.campaignID,
-          recipientID: recipient.id,
-          recipientData: {
-            fields: [
-              {
-                name: 'user_reset_pwd_link',
-                value: reset_link
-              }
-            ]
-          }
-        }, function (err, result) {
-          if (err) { return callback(err); }
-          else {
-            if (result.editRecipient_result.status === 'OK') {
-              callback(null, result.editRecipient_result.id);
-            } else {
-              callback('editRecipient failed');
+        if (recipient) {
+          client.editRecipient({
+            login: config.login,
+            campaignID: config.campaignID,
+            recipientID: recipient.id,
+            recipientData: {
+              fields: fields
             }
-          }
-        });
+          }, function (err, result) {
+            if (err) { return callback(err); }
+            else {
+              if (result.editRecipient_result.status === 'OK') {
+                callback(null, result.editRecipient_result.id);
+              } else {
+                callback('editRecipient failed');
+              }
+            }
+          });
+        } else {
+          client.addRecipient({
+            login: config.login,
+            campaignID: config.campaignID,
+            groupIDs: { 'xsd:int': [81] },
+            recipientData: {
+              fields: fields.push({
+                name: 'email',
+                value: email
+              })
+            }
+          }, function(err, result) {
+            if (err) { return callback(err); }
+            else {
+              if (result.addRecipient_result.status === 'OK') {
+                callback(null, result.addRecipient_result.id);
+              } else {
+                callback('addRecipient failed');
+              }
+            }
+          });
+        }
       },
       function (recipient_id, callback) {
         client.sendSingleMailing({
           login: config.login,
           campaignID: config.campaignID,
-          mailingID: 12,
+          mailingID: config.mailingID,
           recipientID: recipient_id
         }, function (err, result) {
           if (err) { return callback(err); }
@@ -105,5 +133,27 @@ exports.sendStaffResetPwdMail = function (email, uid, host, callback) {
     });
 
   });
+};
 
-}
+
+
+exports.sendStaffResetPwdMail = function (email, uid, host, callback) {
+
+  var reset_link = 'http://' + host + '/users/resetPwd?key=' + encrypt.encrypt(uid, website_config.SECRET) + '&uid=' + uid + '&time=' + encrypt.encrypt(new Date().toString(), website_config.SECRET);
+
+  var reset_config = {
+    login: global_config.login,
+    campaignID: global_config.campaignID,
+    mailingID: global_config.mail.user.reset_pwd
+  };
+
+  sendMail(reset_config, email, [{
+    name: 'user_reset_pwd_link',
+    value: reset_link
+  }], callback);
+
+};
+
+
+
+
