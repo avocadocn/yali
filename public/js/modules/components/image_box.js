20185b245c5862ba165d8c8f1d2ce7b1414d3121
cahavar <cahavar@55yali.com>
2014-10-30 19:20:46 +0800

    添加底部导航

--- public/js/modules/components/image_box.js
+++ public/js/modules/components/image_box.js
@@ -14,11 +14,27 @@ angular.module('donler.components.imageBox', [])
       link: function (scope, ele, attrs, ctrl) {
         var images = scope.images;
         if (images.length > 0) {
+
+          scope.sliderWidth = 0;
+
           scope.isPreview = false;
           scope.prevIndex = 0;
           scope.thisIndex = 0;
           scope.nextIndex = 0;
           scope.previewImg = images[0].uri;
+
+          scope.thumbBoxInnerStyle = {};
+          var width = 60;
+
+          var setMargin = function (index) {
+            var marginWidth = width * index;
+            if (marginWidth >= scope.sliderWidth) {
+              var margin = (0 - marginWidth) + 'px';
+              scope.thumbBoxInnerStyle = {
+                'margin-left': margin
+              };
+            }
+          };
 
           var setIndex = function (index) {
             if (index <= 0) {
@@ -35,7 +51,12 @@ angular.module('donler.components.imageBox', [])
               scope.thisIndex = index;
             }
             scope.previewImg = images[scope.thisIndex].uri;
+            setMargin(scope.thisIndex);
           };
+
+          scope.choose = function (index) {
+            setIndex(index);
+          }
 
           scope.preview = function (index) {
             setIndex(index);
@@ -58,4 +79,16 @@ angular.module('donler.components.imageBox', [])
       }
     };
 
-  });
+  })
+
+  .directive('calWidth', function () {
+    return {
+      restrict: 'A',
+      scope: {
+        resultWidth: '='
+      },
+      link: function (scope, ele, attrs, ctrl) {
+        scope.resultWidth = ele.width();
+      }
+    };
+  })
