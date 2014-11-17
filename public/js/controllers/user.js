103123502d3c28078094127f1930384c7f9a1420
Yali-07 <maggie03230@gmail.com>
2014-11-17 17:46:51 +0800

    bug修复、样式修改

--- public/js/controllers/user.js
+++ public/js/controllers/user.js
@@ -2,19 +2,6 @@
 
 var userApp = angular.module('donler');
 
-userApp.directive('match', function($parse) {
-  return {
-    require: 'ngModel',
-    link: function(scope, elem, attrs, ctrl) {
-      scope.$watch(function() {
-        return $parse(attrs.match)(scope) === ctrl.$modelValue;
-      }, function(currentValue) {
-        ctrl.$setValidity('mismatch', currentValue);
-      });
-    }
-  };
-});
-
 userApp.controller('ActiveController',['$http','$scope',function($http,$scope){
     $scope.active = 0;
     $scope.loading = false;
