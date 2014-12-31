33eabaa783b18218fbe492a3479091c372d94da1
afei <afei@55yali.com>
2014-12-31 13:20:57 +0800

    增加个人积分和活动数脚本

--- database_scripts/141230_update.js
+++ database_scripts/141230_update.js
@@ -34,3 +34,28 @@ photoAlbums.forEach(function (photoAlbum) {
 
 //将官方小队增加poster属性
 db.companygroups.update({poster:{'$exists':false}},{$set:{poster:{role:'HR'}}},{multi:true})
+
+// 2014-12-31 更新个人参加的活动数和积分
+var now = new Date();
+var users = db.users.find();
+users.forEach(function (user) {
+  var campaignCount = db.campaigns.count({
+    'active':true,
+    'cid':user.cid,
+    'campaign_unit.member._id':user._id,
+    'end_time':{'$lte':now}
+  });
+  var teamCount = user.team.length;
+  user.score = {};
+  user.campaignCount = campaignCount;
+  user.score.joinOfficialTeam = teamCount * 5;
+  user.score.officialCampaignSucceded = campaignCount * 10;
+  user.score.quitOfficialTeam = 0;
+  user.score.uploadPhotoToOfficialTeam =0;
+  user.score.total = user.score.officialCampaignSucceded + user.score.joinOfficialTeam;
+  db.users.save(user);
+
+});
+
+
+
