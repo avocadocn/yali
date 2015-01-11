921c702e7ed64313d26de440b674e9a46e786ed0
cahavar <cahavar@55yali.com>
2015-01-11 15:15:03 +0800

    添加修正邀请码的数据库脚本

---
+++ database_scripts/150111_update.js
@@ -0,0 +1,25 @@
+// 更新邀请码为字母和数字，移除特殊字符
+var randomAlphaNumeric = function (len) {
+  var text = "";
+  var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var randomMax = possible.length;
+  for(var i = 0; i < len; i++) {
+    text += possible.charAt(Math.floor(Math.random() * randomMax));
+  }
+  return text;
+}
+
+var companies = db.companies.find();
+companies.forEach(function (company) {
+  company.invite_key = randomAlphaNumeric(8);
+  for (var i = 0; i < company.register_invite_code.length; i++) {
+    var code = company.register_invite_code[i];
+    var rgcode = db.companyregisterinvitecodes.findOne({
+      code: code
+    });
+    rgcode.code = randomAlphaNumeric(8);
+    company.register_invite_code[i] = rgcode.code;
+    db.companyregisterinvitecodes.save(rgcode);
+  }
+  db.companies.save(company);
+});
