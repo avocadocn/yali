ea19057ee31c1e1223d99ad7812b465f25ac2d8f
cahavar <cahavar@55yali.com>
2015-02-08 12:42:22 +0800

    完成活跃度统计饼状图

--- company_manager_client/src/campaign/campaign_directive.js
+++ company_manager_client/src/campaign/campaign_directive.js
@@ -1,4 +1,4 @@
-define(['./campaign', 'echarts', 'echarts/chart/bar'], function (campaign, echarts) {
+define(['./campaign', 'echarts', 'echarts/chart/bar', 'echarts/chart/pie'], function (campaign, echarts) {
   // St: statistics
   return campaign.directive('campaignStBar', [function () {
     return {
@@ -73,30 +73,99 @@ define(['./campaign', 'echarts', 'echarts/chart/bar'], function (campaign, echar
 
     };
   }])
-  .directive('sponsorCampaign', ['campaignService', function (campaignService) {
-    return {
-      restrict: 'E',
-      scope: {
-        data: '='
-      },
-      templateUrl: '/company/manager/templates/campaign/sponsor.html',
-      link: function (scope, ele, attrs, ctrl) {
-        $('#start_time').datetimepicker({
-          autoclose: true,
-          language: 'zh-CN',
-          startDate: new Date(),
-          pickerPosition:"bottom-left"
-        });
-        $('#end_time').datetimepicker({
-          autoclose: true,
-          language: 'zh-CN',
-          startDate: new Date(),
-          pickerPosition:"bottom-left"
-        });
-        scope.sponsorCampaign = function() {
-          console.log(scope.data);
+    .directive('campaignStPie', [function () {
+      return {
+        restrict: 'E',
+        scope: {
+          data: '='
+        },
+        templateUrl: '/company/manager/templates/campaign/st_pie.html',
+        link: function (scope, ele, attrs, ctrl) {
+
+          var formatData = function (data) {
+            var resData = [];
+
+            var max = Math.min(5, data.length);
+            for (var i = 0; i < max; i++) {
+              resData.push({
+                name: '人数统计',
+                type: 'pie',
+                radius: '30%',
+                center: [(10 + 20 * i) + '%', '60%'],
+                data: [
+                  {value: data[i].zero, name: '没有参加'},
+                  {value: data[i].once, name: '参加1次'},
+                  {value: data[i].twice, name: '参加2次'},
+                  {value: data[i].moreThanThreeTimes, name: '3次或以上'}
+                ]
+              });
+            }
+            return resData;
+          };
+
+          scope.$watch('data', function (data) {
+            if (data) {
+
+              var myChart = echarts.init(ele[0].querySelector('.st_pie'));
+
+              var option = {
+                title: {
+                  text: '每周活跃度统计',
+                  x: 'center'
+                },
+                tooltip: {
+                  trigger: 'item',
+                  formatter: "{a} <br/>{b} : {c} ({d}%)"
+                },
+                legend: {
+                  x: 'center',
+                  y: '20%',
+                  data: ['没有参加', '参加1次', '参加2次', '3次或以上']
+                },
+                toolbox: {
+                  show: true,
+                  feature: {
+                    saveAsImage: {show: true}
+                  }
+                },
+                series: formatData(data)
+              };
+
+              myChart.setOption(option);
+
+            }
+          });
+
+
         }
       }
-    }
-  }]);
+    }])
+    .directive('sponsorCampaign', ['campaignService', function (campaignService) {
+      return {
+        restrict: 'E',
+        scope: {
+          data: '='
+        },
+        templateUrl: '/company/manager/templates/campaign/sponsor.html',
+        link: function (scope, ele, attrs, ctrl) {
+          $('#start_time').datetimepicker({
+            autoclose: true,
+            language: 'zh-CN',
+            startDate: new Date(),
+            pickerPosition:"bottom-left"
+          });
+          $('#end_time').datetimepicker({
+            autoclose: true,
+            language: 'zh-CN',
+            startDate: new Date(),
+            pickerPosition:"bottom-left"
+          });
+          scope.sponsorCampaign = function() {
+            console.log(scope.data);
+          }
+        }
+      }
+    }]);
 });
+
+
