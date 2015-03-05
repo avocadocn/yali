9d79823ce68a82815d939c1e376ac68fa5563227
afei <afei@55yali.com>
2015-03-05 15:26:40 +0800

    增加小队的胜率纪录

--- app/models/company_group.js
+++ app/models/company_group.js
@@ -188,6 +188,18 @@ var CompanyGroup = new Schema({
         rank:{
             type: Number,
             default: 0
+        },
+        win:{
+            type: Number,
+            default: 0
+        },
+        tie:{
+            type: Number,
+            default: 0
+        },
+        lose:{
+            type: Number,
+            default: 0
         }
     }
 });
