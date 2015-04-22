7474503034fb65169ab369fab2422ebc49c44b05
cahavar <cahavar@55yali.com>
2015-04-22 11:37:09 +0800

    修改图标

--- config/express.js
+++ config/express.js
@@ -143,7 +143,7 @@ module.exports = function (app, passport, db) {
     app.use(app.router);
 
     // Setting the fav icon and static folder
-    app.use(express.favicon());
+    app.use(express.favicon(config.root + '/public/img/icons/favicon.ico'));
     app.use(express.static(config.root + '/public'));
 
     app.use(errorHandle);
