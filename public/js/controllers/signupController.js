ea46ea861fc12039e44d8741e49a2506addd270b
Maggie <maggie03230@gmail.com>
2015-09-06 20:47:29 +0800

    手机注册页逻辑及样式完成

--- public/js/controllers/signupController.js
+++ public/js/controllers/signupController.js
@@ -3,63 +3,76 @@
 var app = angular.module('donler', []);
 var apiBaseUrl = 'http://' + window.location.hostname + ':3002'+'/v2_0';
 
-app.controller('userSignupController', ['$http', '$scope', '$location', function($http, $scope, $location) {
-  //----------------------- step 1 找学校
-  $scope.step = 1;
-  $scope.showCompanies = false;
+app.controller('userSignupController', ['$http', '$scope', '$location', '$timeout', function($http, $scope, $location, $timeout) {
   $http.post(apiBaseUrl + '/search/companies',{name:'上海'})
   .success(function(data, status) {
     $scope.companies = data.companies;
-    $scope.showCompanies = true;
   })
 
-  $scope.search = function(keyEvent) {
-    if($scope.keyword && (keyEvent.which === 13 || !keyEvent)) {
-      $http.post(apiBaseUrl + '/search/companies',{name:$scope.keyword})
-      .success(function(data, status) {
-        $scope.companies = data.companies;
-      })
-      .error(function(data, status) {
-        $scope.companies = [];
-      })
-    }
+  var years = [];
+  for(var i = 2015; i>=1980; i--) {
+    years.push(i);
+  }
+  $scope.years = years;
+  $scope.user = {gender: false};
+
+  $scope.selectCompany = function() {
+    $scope.user.cid = $scope.company._id;
   };
 
-  $scope.select = function(company) {
-    $scope.step = 2;
-    $scope.company = company;
-  };
+  $scope.countDown = '';
 
-  //----------------------- step 2 输入资料
-
-  $scope.goto = function(step) {
-    $scope.step = step;
-  };
-
-  //----------------------- step 3 验证手机
-
-  $scope.isValidating = false;
   $scope.validatePhone = function() {
     $scope.isValidating = true;
-    $http.post('/users/validate', {
-      phone:$scope.user.phone,
+    $http.post(apiBaseUrl + '/users/validate', {
+      phone: $scope.user.phone,
       from: 'website'
     })
     .success(function(data, status) {
-      $scope.validated = true;
+      if(data.active) {
+        $scope.tip = '此手机号已被注册！'
+      }
+      else {
+        $scope.tip = '';
+        $scope.validated = true;
+        //倒计时
+        $scope.countDown = 60;
+        var myTime = setInterval(function() {
+          $scope.countDown--;
+          $scope.$digest();
+          if($scope.countDown<=0) {
+            $scope.countDown = null;
+            $scope.$digest();
+            clearInterval(myTime);
+          }
+        }, 1000);
+      }
     })
     .error(function(data, status) {
-      $scope.tip = '验证失败，请重试';
+      $scope.tip = data.msg;
     })
   };
 
   $scope.register = function() {
-    $http.post(apiBaseUrl + '/users', $scope.user).success(function(data, status) {
-      $location.path('/signup/success');
-    })
-    .error(function(data, status) {
-      $location.path('/signup/fail');
-    })
-  }
-  
+    if(!$scope.user.cid) {
+      $scope.errorMSg = '请选择你的学校';
+    }
+    else if(!$scope.user.enrollment>1970) {
+      $scope.errorMSg = '请选择入学时间';
+    }
+    else {
+      var sendData = $scope.user;
+      sendData.from = 'website';
+      $http.post(apiBaseUrl + '/users', sendData).success(function(data, status) {
+        var hostname = window.location.hostname;
+        if(window.location.hostname === 'localhost') {
+          hostname = hostname + ':3000';
+        }
+        window.location = 'http://' + hostname + '/singup/success';
+      })
+      .error(function(data, status) {
+        $scope.errorMSg = data.msg;
+      })
+    }
+  };
 }])
